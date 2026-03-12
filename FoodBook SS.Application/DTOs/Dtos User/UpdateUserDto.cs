c05b61084858b504d3e6cc5ae2bf18b310b17c79
Sandro Steven De Oleo De Los Santos <20251148@itla.edu.do>
2026-03-11 20:53:07 -0400

    Dto y DEp

---
+++ FoodBook SS.Application/DTOs/Dtos User/UpdateUserDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodBook_SS.Application.DTOs.Dtos_User
+{
+    internal class UpdateUserDto
+    {
+    }
+}
